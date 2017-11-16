ad76a3f61dec52a841c824586805a225e159bdc7
Thomas Rutzer <thomas.rutzer@gmx.de>
2017-11-16 22:47:18 +0100

    basic create page

---
+++ src/components/create/create.component.ts
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+@Component({
+    template: require('./create.component.html')
+})
+export class CreateComponent extends Vue {
+
+}
