89943490f24741fa2e9893cfd35f032a059b4f28
Thomas Rutzer <thomas.rutzer@gmx.de>
2017-11-16 22:49:29 +0100

    math utils

---
+++ src/components/generator/manager/GeneratorManager.ts
@@ -0,0 +1,26 @@
+import GlobalLight from './../../light/factory/GlobalLightFactory';
+import SceneObjectModel from './../../scene/model/SceneObjectModel';
+import { MountainFactory } from './../../mountain/index';
+
+export default class GeneratorManager {
+    private scene;
+    private mountain;
+    private globalLight;
+
+    constructor(scene) {
+        this.scene = scene;
+        this.addGlobalLight();
+
+        this.addMountain();
+    }
+
+    addGlobalLight() {
+        this.globalLight = new GlobalLight('#fff', '#fff', 0.8);
+        this.scene.addElement(SceneObjectModel.create('globalLight', this.globalLight.lightElement));
+    }
+
+    addMountain() {
+        this.mountain = SceneObjectModel.create('mountain', MountainFactory.create(30, 15).mesh, {y: 0, x:0});
+        this.scene.addElement(this.mountain);
+    }
+}
---
+++ src/generics/utils/index.ts
@@ -0,0 +1,5 @@
+import * as mathUtils from './mathUtils';
+
+export {
+    mathUtils
+}
